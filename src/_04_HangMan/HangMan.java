19dfceb6ef1f4c977faf3ade774b9871b0f46e92
zhaoa1262 <league@WTS-IM-6.attlocal.net>
2019-03-03 11:28:14 -0800

    HangMan IP

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -23,19 +23,38 @@ public class HangMan implements KeyListener{
 	
 	Stack<String> HangWords;
 	
+	char ch = '~';
+	
+	boolean changeWordInGameScreen = true;
+	boolean resetGameScreen = true;
+	
+	ArrayList<Boolean> b = new ArrayList();
 	public static void main(String[] args) {
 		new HangMan().Order();
 	}
 	
-	void gameScreen(String s) {
+	void gameScreen(String s, int j,ArrayList<Boolean> bo) {
 		f = new JFrame();
 		p = new JPanel();
 		l = new JLabel();
+		
+		String sl = "";
+		
+		for(int i = 0; i < s.length(); i++) {
+			if(bo.get(i) == true) {
+				sl += s.charAt(i);
+			}else {
+				sl += "_";
+			}
+		}
+		
+		l.setText(sl + "   Lives Remaining: " + j);
 		
 		p.add(l);
 		
 		f.add(p);
 		f.pack();
+		f.addKeyListener(this);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setTitle("HANGman");
@@ -43,45 +62,65 @@ public class HangMan implements KeyListener{
 	}
 	
 	void Order() {
-		int b = 1;
-		while(b == 1) {
+		int loop = 1;
+		int lives = 10;
+		String s = "";
+		while(loop == 1) {
 			if(jopon == 0) {
 				input = inpuT();
 			}if(input > 0 && input < 51) {
 				jopon = 1;
 			}if(jopon == 1) {
 				HangWords = new Stack<String>();
-				String s = "";
-				boolean StackIn = false;
+				s= "";
 				for(int i = 0; i < input; i++) {
-					while(StackIn == false) {
-						System.out.println("hi");
-					    s = Utilities.readRandomLineFromFile("dictionary.txt");
-					    if(HangWords.contains(s) == false) {
-					        HangWords.push(s);
-					      	StackIn = true;
-					    }
+					s = Utilities.readRandomLineFromFile("dictionary.txt");
+					if(HangWords.contains(s) == false) {
+					    HangWords.push(s);
+					}else {
+					    	i--;
 					}
 				}
+				jopon = 2;
 			}if(jopon == 2) {
-				b = 2;
+				loop = 2;
+				jopon = 0;
+				lives = 10;
 			}
 		}
 		System.out.println("got Input");
-		while(b == 2) {
-			for(int i = 0; i < HangWords.size(); i++) {
-				System.out.println(HangWords.elementAt(i));
+		while(loop == 2) {
+			if(changeWordInGameScreen) {
+				s = HangWords.pop();
+				changeWordInGameScreen = false;
+				b.clear();
+				for(int i = 0; i < s.length(); i++) {
+					b.add(false);
+				}
+			}			
+			if (resetGameScreen == true){
+				System.out.println(ch);
+				if(ch != '~') {
+					for(int i = 0; i < s.length(); i++) {
+						if(s.charAt(i) == ch) {
+							b.set(i, true);
+						}
+					}
+				}
+				gameScreen(s,lives,b);
+				resetGameScreen = false;
 			}
+			
 		}
 	}
 	
 	public int inpuT() {
 		int i = 0;
-		String s = "111";
-			while(i < 0||i > 50||s.length() > 2) {
+		String JOp = "111";
+			while(i < 0||i > 50||JOp.length() > 2) {
 				try{					
-					s = JOptionPane.showInputDialog("Welcome to HANGMAN \n Please enter a number Between 0 and 50 \n For how many words you would like to guess");
-					i = Integer.parseInt(s);
+					JOp = JOptionPane.showInputDialog("Welcome to HANGMAN! \n Please enter a number Between 0 and 50 \n For how many words you would like to guess");
+					i = Integer.parseInt(JOp);
 					return i;
 				}catch(NumberFormatException e) {
 					error();
@@ -91,7 +130,7 @@ public class HangMan implements KeyListener{
 	}
 	
 	void error() {
-		JOptionPane.showMessageDialog(null, "please enter a number between 0 & 50");
+		JOptionPane.showMessageDialog(null, "Please enter a number between 0 & 50");
 	}
 	
 	void winLose() {
@@ -109,7 +148,9 @@ public class HangMan implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
+		char c = e.getKeyChar();
+		ch = c;
+		resetGameScreen = true;
 	}
 
 	@Override
