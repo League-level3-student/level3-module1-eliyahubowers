fbbd843f9204c1a18fee7775db1b8c0ad05be490
eliyahubowers <league@WTS-IM-6.attlocal.net>
2019-03-10 11:28:26 -0700

    Hangman IP

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -26,19 +26,31 @@ public class HangMan implements KeyListener{
 	char ch = '~';
 	
 	boolean changeWordInGameScreen = true;
-	boolean resetGameScreen = true;
+	boolean doResetGameScreen = true;
 	
 	ArrayList<Boolean> b = new ArrayList();
 	public static void main(String[] args) {
 		new HangMan().Order();
 	}
 	
-	void gameScreen(String s, int j,ArrayList<Boolean> bo) {
+	void gameScreen() {
 		f = new JFrame();
 		p = new JPanel();
 		l = new JLabel();
 		
-		String sl = "";
+		p.add(l);
+		
+		f.add(p);
+		
+		f.pack();
+		f.addKeyListener(this);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setTitle("HANGman");
+	}
+	
+	void resetGameScreen(String s, int j, ArrayList<Boolean> bo) {
+        String sl = "";
 		
 		for(int i = 0; i < s.length(); i++) {
 			if(bo.get(i) == true) {
@@ -54,64 +66,176 @@ public class HangMan implements KeyListener{
 		
 		f.add(p);
 		f.pack();
-		f.addKeyListener(this);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setTitle("HANGman");
-		f.addKeyListener(this);
+		
 	}
 	
 	void Order() {
-		int loop = 1;
+		
+		boolean won = false;
 		int lives = 10;
+		int loop = 1;
 		String s = "";
+		
 		while(loop == 1) {
+			
 			if(jopon == 0) {
+				
 				input = inpuT();
+				
 			}if(input > 0 && input < 51) {
+				
 				jopon = 1;
+				
 			}if(jopon == 1) {
+				
 				HangWords = new Stack<String>();
 				s= "";
+				
 				for(int i = 0; i < input; i++) {
+					
 					s = Utilities.readRandomLineFromFile("dictionary.txt");
+					
 					if(HangWords.contains(s) == false) {
+						
 					    HangWords.push(s);
+					    
 					}else {
+						
 					    	i--;
+					    	
 					}
+					
 				}
+				
 				jopon = 2;
+				
 			}if(jopon == 2) {
-				loop = 2;
+				
 				jopon = 0;
-				lives = 10;
-			}
-		}
-		System.out.println("got Input");
-		while(loop == 2) {
-			if(changeWordInGameScreen) {
-				s = HangWords.pop();
-				changeWordInGameScreen = false;
-				b.clear();
-				for(int i = 0; i < s.length(); i++) {
-					b.add(false);
-				}
-			}			
-			if (resetGameScreen == true){
-				System.out.println(ch);
-				if(ch != '~') {
-					for(int i = 0; i < s.length(); i++) {
-						if(s.charAt(i) == ch) {
-							b.set(i, true);
-						}
-					}
-				}
-				gameScreen(s,lives,b);
-				resetGameScreen = false;
+				lives = 10;               
+				loop = 2;
+				
+				gameScreen();
+				
 			}
 			
 		}
+		
+		System.out.println("loop2");
+		
+		while(loop == 2) {
+			
+			System.out.print("");
+			
+			if(changeWordInGameScreen == true) {
+				
+				if(HangWords.isEmpty() == false) {
+				
+					s = HangWords.pop();
+					changeWordInGameScreen = false;
+             	    b.clear();
+             	    lives = 10;
+                
+				}else{
+					
+					won = true;
+					loop = 3;
+					
+				}
+				
+                for(int i = 0; i < s.length(); i++) {
+                	
+                		b.add(false);
+					
+			    }
+                
+			}	
+			
+			if (doResetGameScreen == true){
+				
+				if(ch != '~') {
+					
+					boolean check = false;
+					
+					for(int i = 0; i < s.length(); i++) {
+						
+						if(s.charAt(i) == ch) {
+							
+							b.set(i, true);
+							check = true;
+							
+						}
+						
+					}
+					
+					if(check == false) {
+						lives --;
+					}
+					
+					ch = '~';
+					
+				}
+				// runs screen
+				resetGameScreen(s,lives,b);
+				doResetGameScreen = false;
+				//
+				for(int i = 0; i < s.length(); i++) {
+					
+					if(b.get(i) != true) {
+						i = s.length();
+					}
+					
+					if(i == s.length()-1) {
+						
+						changeWordInGameScreen = true;
+						
+					}
+					
+				}
+				
+			}
+			
+			if(lives == 0) {
+				
+				won = false;
+				loop = 3;
+				
+			}
+			
+		}
+		
+		while(loop == 3) {
+			
+			String ss = "";
+			
+			if(won == true) {
+				
+				ss = "You Won!!!";
+				
+			}else {
+				
+				ss = "You ran out of lives \nThe word was: " + s;
+				
+			}
+			
+			JOptionPane.showMessageDialog(null, ss);
+			int continu = JOptionPane.showConfirmDialog(null, "continuePlaying ?");
+			//cancel = 2 no = 1 yes = 0
+			System.out.println(continu);
+			
+			if(continu == 0) {
+				
+				loop = 1;
+				jopon = 0;
+				
+			}else{
+				
+				System.exit(0);
+				
+			}
+			
+		}
+		
 	}
 	
 	public int inpuT() {
@@ -132,14 +256,6 @@ public class HangMan implements KeyListener{
 	void error() {
 		JOptionPane.showMessageDialog(null, "Please enter a number between 0 & 50");
 	}
-	
-	void winLose() {
-		
-	}
-	
-	void playAgain() {
-		
-	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -150,7 +266,7 @@ public class HangMan implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		char c = e.getKeyChar();
 		ch = c;
-		resetGameScreen = true;
+		doResetGameScreen = true;
 	}
 
 	@Override
